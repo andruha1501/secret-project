777860d4cf9e0fdc30bb50b5792e267a7b58fcaa
yuracr9 <fischuk1996@gmail.com>
2017-09-11 16:51:57 +0300

    add map

---
+++ src/app/add-house/add-house.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'add-house',
+  templateUrl: './add-house.component.html',
+  styleUrls: ['./add-house.component.css']
+})
+export class AddHouseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+  }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+declare let google: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  ngOnInit() {
+    let map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      scrollwheel: true,
+      zoom: 16
+    });
+
+    let infoWindow = new google.maps.InfoWindow;
+
+    if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('This is you.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+      });
+    }
+  }
 }
