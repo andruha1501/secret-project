073b8810ba2dad254b47a6a75bd16cba8a43272e
Andriy <andruha1501@gmail.com>
2017-09-11 23:17:08 +0300

    display maps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -28,7 +28,27 @@ export class AppComponent implements OnInit {
   title = 'app';
   state: string = "right";
   ngOnInit() {
+    let map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: -34.397, lng: 150.644},
+      scrollwheel: true,
+      zoom: 16
+    });
 
+    let infoWindow = new google.maps.InfoWindow;
+
+    if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('This is you.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+      });
+    }
   }
   toggleState(): void {
     this.state = (this.state === 'right' ? 'left' : 'right');
