53e2c5781daa40b1c20565bd5b5527cc03c6fb28
Andriy <andruha1501@gmail.com>
2017-09-19 15:07:57 +0300

    own geolocation

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,14 +1,17 @@
+//https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/googlemaps
+
 import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef, NgZone } from '@angular/core';
 import {} from '@types/googlemaps';
 import { MapService } from '../services/map.service';
+
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.scss']
 })
 export class MapComponent implements OnInit {
-  @Input('lat') public lat: number;
-  @Input('lng') public lng: number;
+ // @Input('lat') public lat: number;
+ // @Input('lng') public lng: number;
   @ViewChild('mapContainer') mapContainer: ElementRef;
 
   //public markers: google.maps.Marker[] = []; 
@@ -16,7 +19,7 @@ export class MapComponent implements OnInit {
   constructor(private mapService: MapService) { }
 
   ngOnInit() {
-    let latlng = new google.maps.LatLng(this.lat,this.lng);
+    let latlng = new google.maps.LatLng(	48.29257, 25.93585);
     this.mapService.map = new google.maps.Map(this.mapContainer.nativeElement,{
       center: latlng,
       zoom: 15,
@@ -24,13 +27,22 @@ export class MapComponent implements OnInit {
       disableDefaultUI: true,
       zoomControl: true
     });
+    if(navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        let pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        }
+        this.mapService.map.setCenter(pos);
+      });
+    }
     this.getData();
   }
   getData(): void {
     this.mapService
         .getData()
         .subscribe(markers => {
-          for(var m of markers)
+          for(let m of markers)
             this.mapService.addMarker(m.lat, m.lng);
 
         });
